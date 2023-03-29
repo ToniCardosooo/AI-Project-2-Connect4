51f6388e73e30c95522876009bbb6c2df4404239
Barbara Santos <blsbls03@gmail.com>
2023-03-29 11:59:25 +0100

    Update Heuristics.java

--- Heuristics.java
+++ Heuristics.java
@@ -82,11 +82,108 @@ public class Heuristics {
         for (int i = 0; i < 7; i++) {
             count += b.getPiecesCol()[i];
         }
+
         if (count > 6) {
             if (win(b) != 0) return win(b)*512;
         }
 
-        return 0;
+        int[][] bb = b.getBoard();
+        int count1, count2;
+        int score = 0;
+
+        //horizontal
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 4; j++) {
+                count1 = 0;
+                count2 = 0;
+                if (bb[i][j] == 1) count1++;
+                if (bb[i][j+1] == 1) count1++;
+                if (bb[i][j+2] == 1) count1++;
+                if (bb[i][j+3] == 1) count1++;
+                if (bb[i][j] == 2) count2++;
+                if (bb[i][j+1] == 2) count2++;
+                if (bb[i][j+2] == 2) count2++;
+                if (bb[i][j+3] == 2) count2++;
+
+                if (count1 != 0 && count2 != 0) continue;
+                if (count1 == 1) score++;
+                if (count2 == 1) score--;
+                if (count1 == 2) score+=10;
+                if (count2 == 2) score-=10;
+                if (count1 == 3) score+=50;
+                if (count2 == 3) score-=50;
+            }
+        }
+
+        //vertical
+        for (int i = 3; i < 6; i++) {
+            for (int j = 0; j < 7; j++) {
+                count1 = 0;
+                count2 = 0;
+                if (bb[i][j] == 1) count1++;
+                if (bb[i-1][j] == 1) count1++;
+                if (bb[i-2][j] == 1) count1++;
+                if (bb[i-3][j] == 1) count1++;
+                if (bb[i][j] == 2) count2++;
+                if (bb[i-1][j] == 2) count2++;
+                if (bb[i-2][j] == 2) count2++;
+                if (bb[i-3][j] == 2) count2++;
+
+                if (count1 != 0 && count2 != 0) continue;
+                if (count1 == 1) score++;
+                if (count2 == 1) score--;
+                if (count1 == 2) score+=10;
+                if (count2 == 2) score-=10;
+                if (count1 == 3) score+=50;
+                if (count2 == 3) score-=50;
+            }
+        }
+
+        // diagonals
+        for (int i = 3; i < 6; i++) {
+            //if col tem menos de 4 continue 
+            for (int j = 0; j < 4; j++) {
+                count1 = 0;
+                count2 = 0;
+                if (bb[i][j] == 1) count1++;
+                if (bb[i-1][j+1] == 1) count1++;
+                if (bb[i-2][j+2] == 1) count1++;
+                if (bb[i-3][j+3] == 1) count1++;
+                if (bb[i][j] == 2) count2++;
+                if (bb[i-1][j+1] == 2) count2++;
+                if (bb[i-2][j+2] == 2) count2++;
+                if (bb[i-3][j+3] == 2) count2++;
+
+                if (count1 != 0 && count2 != 0) continue;
+                if (count1 == 1) score++;
+                if (count2 == 1) score--;
+                if (count1 == 2) score+=10;
+                if (count2 == 2) score-=10;
+                if (count1 == 3) score+=50;
+                if (count2 == 3) score-=50;
+
+                count1 = 0;
+                count2 = 0;
+                if (bb[i-3][j] == 1) count1++;
+                if (bb[i-2][j+1] == 1) count1++;
+                if (bb[i-1][j+2] == 1) count1++;
+                if (bb[i][j+3] == 1) count1++;
+                if (bb[i-3][j] == 2) count2++;
+                if (bb[i-2][j+1] == 2) count2++;
+                if (bb[i-1][j+2] == 2) count2++;
+                if (bb[i][j+3] == 2) count2++;
+
+                if (count1 != 0 && count2 != 0) continue;
+                if (count1 == 1) score++;
+                if (count2 == 1) score--;
+                if (count1 == 2) score+=10;
+                if (count2 == 2) score-=10;
+                if (count1 == 3) score+=50;
+                if (count2 == 3) score-=50;
+            }
+        }
+
+        return score;
     }
 
     public static int getScore(Board b){
