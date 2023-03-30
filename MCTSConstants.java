bf9e67a89ccd3a8dd6c50c277aaa70b0b72d30bb
António Cardoso <antoniocardosopessoal@gmail.com>
2023-03-31 00:35:14 +0100

    MCTS finished

--- MCTSConstants.java
+++ MCTSConstants.java
@@ -1,5 +1,5 @@
 public class MCTSConstants {
-    final static public int MCTS_ITERATIONS = 50000;
+    final static public int MCTS_ITERATIONS = 75000;
     final static public double EXPLORATION = Math.sqrt(2);
     final static public int INF = Integer.MAX_VALUE;
 }
