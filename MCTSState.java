f924e66b3ae275a154c9d4def54ed5b66ed50145
António Cardoso <102590166+ToniCardosooo@users.noreply.github.com>
2023-04-01 22:44:42 +0100

    Update MCTSState.java

--- MCTSState.java
+++ MCTSState.java
@@ -1,24 +1,5 @@
 import java.util.PriorityQueue;
 
-/*
- * Ideas
- * 
- * - Each node has a priority queue that saves the reference to its children
- *   and orders it based on the game policy (heuristic)
- * 
- * - In the Backpropagation phase, to update the Priority Queue of the node's children
- *   we can remove and insert the head of the PQ in order to reorder it based on the
- *   backprop results
- * 
- * - In the rool-out phase we CANNOT save the roll-out states in memory. The roll-out should
- *   happen in a single varible that updates itself to each next move until the player wins, draws or loses.
- * 
- * - The class that will be developed to hold the MCTS algorithm will need an
- *   atribute that represents how many cycles of "Selection, Expantion, Roll-Out, BackProp"
- *   the algorithm will use
- * 
- */
-
 public class MCTSState implements Comparable<MCTSState>{
 
     // atributes
